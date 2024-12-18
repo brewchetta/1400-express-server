f6237250324933a2c981772791e4708c86070e00
brewchetta <chett.tiller@gmail.com>
2024-12-18 14:15:24 -0500

    remove commented ancestry routes

--- routes/ancestries.js
+++ routes/ancestries.js
@@ -16,42 +16,4 @@ router.get('/', async (req, res, next) => {
 });
 
 
-/* FORBIDDEN ROUTES */
-
-
-/* POST /ancestries */
-// router.post('/', async (req, res, next) => {
-//   const newAncestry = await Ancestry.create(req.body)
-//   res.status(201).json({ status: 201, message: "Success", result: newAncestry })
-// })
-
-
-// /* PATCH /ancestries */
-// router.patch('/:name', async (req, res, next) => {
-//   const ancestry = await Ancestry.findOne({name: req.params.name}).exec()
-//   if (checkExistence(ancestry, res, next)) {
-//     try {
-//       // iterate on keys and update
-//       Object.keys(req.body).forEach(key => ancestry[key] = req.body[key] )
-//       // save and return
-//       await ancestry.save()
-//       res.status(202).json(ancestry)
-//     } catch (error) {
-//       res.status(400)
-//       next(error)
-//     }
-//   }
-
-// })
-
-
-// router.delete('/:name', async (req, res, next) => {
-//   const ancestry = await Ancestry.findOne({name: req.params.name}).exec()
-//   if (checkExistence(ancestry, res, next)) {
-//     await Ancestry.deleteOne({name: toTitleCase(req.params.name)})
-//     res.status(202).json({status: 202, message: "Deleted", item: ancestry})
-//   }
-// })
-
-
 export default router
